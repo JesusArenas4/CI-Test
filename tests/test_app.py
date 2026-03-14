242e56265906f1f35a67bfaa471bd16d5ea5060b
jesus arenas <jesus.arenas@correounivalle.edu.co>
2026-03-14 10:40:01 -0500

    pruebas 2.0

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,12 @@
 from app import calcular_prioridad
 
+
 def test_prioridad_normal():
     assert calcular_prioridad(3) == "prioridad normal"
 
 
 def test_prioridad_alta():
     assert calcular_prioridad(6) == "prioridad alta"
-
 
 
 def test_dias_negativos():
